f29d2a997169ab645b9f3fa1e754dcb51f5206e3
Ka Chun (Franco) Mo <46771981+francomomo@users.noreply.github.com>
2020-09-21 17:19:52 -0400

    Initial Commit

---
+++ Algo Visualizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Algo Visualizer
+//
+//  Created by Franco Mo on 2020/9/21.
+//  Copyright © 2020 francomomo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
